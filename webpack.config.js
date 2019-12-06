be68ebcbce0900253b89bb5bd49f0a595ce8d853
weiyongjian <Weiyongjian123!>
2019-12-06 16:39:16 +0800

    ..

---
+++ webpack.config.js
@@ -0,0 +1,118 @@
+const path = require("path"),
+  uglifyjsWebpackPlugin = require("uglifyjs-webpack-plugin"),
+  htmlWebpackPlugin = require("html-webpack-plugin"),
+  autoprefixer = require("autoprefixer"),
+  miniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+  mode: "development", // development 有注释  production  完全压缩
+  entry: {
+    //入口文件
+    index: path.resolve(__dirname, "./src/js/index.js"),
+    jquery: path.resolve(__dirname, "./src/js/jquery.min.js")
+  },
+  output: {
+    //输出
+    path: path.resolve(__dirname + "/dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      //规则
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: path.resolve(__dirname, "node_modules"), //忽略
+        query: {
+          presets: ["latest"]
+        }
+      },
+
+      {
+        test: /\.tpl$/,
+        loader: "ejs-loader"
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: function() {
+                return [autoprefixer("last 5 versions")];
+              }
+            }
+          },
+          "sass-loader"
+        ]
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: miniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader"
+        ]
+      },
+
+      {
+        test: /\.(png|jpg|jpeg|gif|ico)$/i,
+        loader: [
+          "url-loader?limit=1024&name=img/[name]-[hash:16].[ext]",
+          "image-webpack-loader"
+        ]
+      },
+
+      {
+        test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
+        loader: "url-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    //插件
+    new uglifyjsWebpackPlugin(),
+    new htmlWebpackPlugin({
+      minify: {
+        removeComment: true,
+        collapseWhitespace: true
+      },
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/index.html"),
+      title: "jsModule ",
+      chunksSortMode: "manual",
+      chunks: ["jquery", "index"],
+      excludeChunks: ["node_modules"],
+      hash: true
+    }),
+    new miniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ],
+
+  devServer: {
+    watchOptions: {
+      //监听
+      ignored: /node_modules/ //忽略
+    },
+    open: true,
+    host: "localhost",
+    port: "3000"
+  }
+};
+
+module.exports = config;
